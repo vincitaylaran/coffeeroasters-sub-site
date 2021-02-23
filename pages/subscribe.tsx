d3b4b42258ee065bdf66f1c4472462a3c1de3b92
Vinci Taylaran <svl.taylaran@gmail.com>
2021-02-23 16:28:30 -0700

    Create subscribe page and sass file

--- pages/subscribe.tsx
+++ pages/subscribe.tsx
@@ -1,10 +1,16 @@
-import React from "react"
+import Hero from "../components/Hero"
+import subscribeStyles from "../styles/Subscribe.module.scss"
 
 const subscribe = () => {
   return (
-    <div>
-      <h1>Subscribe</h1>
-    </div>
+    <>
+      <Hero
+        background="blackcup"
+        title="Create a plan"
+        content="Build a subscription plan that best fits your needs. We offer an assortment of the best 
+  artisan coffees from around the globe delivered fresh to your door."
+      />
+    </>
   )
 }
 
