2592b3c702a259971e6022749ed3dd6bcbaf6a43
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-02 10:25:30 -0700

    Add styles for <OrderSummary />

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -1,14 +1,26 @@
+import { spawn } from "child_process"
+import orderSummaryStyles from "../styles/OrderSummary.module.scss"
+
 interface Props {
   answers: string[]
 }
 
+const Answer = ({ ...props }) => {
+  return <span className={orderSummaryStyles.answer}>{props.answer}</span>
+}
+
 const OrderSummary = ({ answers }: Props) => {
   return (
-    <div>
+    <div className={orderSummaryStyles.summary}>
+      <span>ORDER SUMMARY</span>
       {answers[0] === "Capsule" ? (
-        <p>{`"I drink my coffee using ${answers[0]}, with a ${answers[1]} type of bean. ${answers[2]}, sent to me ${answers[4]}"`}</p>
+        <p
+          className={orderSummaryStyles.content}
+        >{`"I drink my coffee using ${answers[0]}, with a ${answers[1]} type of bean. ${answers[2]}, sent to me ${answers[4]}"`}</p>
       ) : (
-        <p>{`"I drink my coffee as ${answers[0]}, with a ${answers[1]} type of bean. ${answers[2]} ground ala ${answers[3]}, sent to me ${answers[4]}"`}</p>
+        <p
+          className={orderSummaryStyles.content}
+        >{`"I drink my coffee as ${answers[0]}, with a ${answers[1]} type of bean. ${answers[2]} ground ala ${answers[3]}, sent to me ${answers[4]}"`}</p>
       )}
     </div>
   )
