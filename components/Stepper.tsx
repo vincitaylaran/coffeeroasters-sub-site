b7c476427c9a5cab34783995c97b489172c0e0b7
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-03 15:05:49 -0700

    Create inline styles in <Stepper /> to change step opacity to 0.2 if first answer is "Capsules"

--- components/Stepper.tsx
+++ components/Stepper.tsx
@@ -4,15 +4,16 @@ import { deepClone } from "../utils"
 
 interface Props {
   currentStep: number
+  answers: string[]
   onStep?: (stepNum: number) => void
 }
 
-const Stepper = ({ currentStep, onStep }: Props) => {
+const Stepper = ({ currentStep, onStep, answers }: Props) => {
   const [steps, setSteps] = useState<any[]>([
     { title: "Preferences", isCurrentStep: false },
     { title: "Bean Type", isCurrentStep: false },
     { title: "Quantity", isCurrentStep: false },
-    { title: "Grind Option", isCurrentStep: false },
+    { title: "Grind Option", isCurrentStep: false, isDisabled: false },
     { title: "Deliveries", isCurrentStep: false },
   ])
 
@@ -23,20 +24,28 @@ const Stepper = ({ currentStep, onStep }: Props) => {
         ? { title: step.title, isCurrentStep: true }
         : { title: step.title, isCurrentStep: false }
     )
+    stepsCopy[3].isDisabled = answers[0] === "Capsule"
     setSteps(stepsCopy)
-  }, [currentStep])
+  }, [currentStep, answers[0]])
 
   return (
     <ul className={stepperStyles.steps}>
       {steps.map((step, i) => (
         <li
+          style={{
+            // Opacity will not go down to 0.2 if apply the step__disabled class for some reason so I decided to do it inline
+            opacity:
+              answers[0] === "Capsule" && step.title === "Grind Option"
+                ? 0.2
+                : "",
+          }}
           key={i}
           onClick={() => onStep(i)}
-          className={
+          className={`${
             step.isCurrentStep
               ? stepperStyles.step__current
               : stepperStyles.step
-          }
+          } }`}
         >
           <a href={`#panel-${i}`}>
             <span className={stepperStyles.stepNum}>0{i + 1} </span>
