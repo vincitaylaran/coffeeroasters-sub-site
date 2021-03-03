4a3ad31a1dbc64834be8cc6686c0fad226ad1060
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-03 13:07:07 -0700

    Update <OrderSummary /> styles

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -1,27 +1,51 @@
-import { spawn } from "child_process"
 import orderSummaryStyles from "../styles/OrderSummary.module.scss"
 
 interface Props {
   answers: string[]
 }
 
-const Answer = ({ ...props }) => {
-  return <span className={orderSummaryStyles.answer}>{props.answer}</span>
+export const Content = ({ answers }: Props) => {
+  return (
+    <div className={orderSummaryStyles.content}>
+      <span>
+        "I drink my coffee {answers[0] === "Capsule" ? "using" : "as"}{" "}
+      </span>
+      <span className={orderSummaryStyles.answer}>
+        {answers[0] ? answers[0] : "_____"}
+      </span>
+      <span>, with a </span>
+      <span className={orderSummaryStyles.answer}>
+        {answers[1] ? answers[1] : "_____"}
+      </span>
+      <span> type of bean. </span>
+      <span className={orderSummaryStyles.answer}>
+        {answers[2] ? answers[2] : "_____"}
+      </span>
+
+      {answers[0] !== "Capsule" && (
+        <>
+          <span> ground ala </span>
+          <span className={orderSummaryStyles.answer}>
+            {answers[3] ? answers[3] : "_____"}
+          </span>
+        </>
+      )}
+
+      <span>, sent to me </span>
+      <span className={orderSummaryStyles.answer}>
+        {answers[4] ? answers[4] : "_____"}
+      </span>
+      <span>."</span>
+    </div>
+  )
 }
 
 const OrderSummary = ({ answers }: Props) => {
   return (
-    <div className={orderSummaryStyles.summary}>
-      <span>ORDER SUMMARY</span>
-      {answers[0] === "Capsule" ? (
-        <p
-          className={orderSummaryStyles.content}
-        >{`"I drink my coffee using ${answers[0]}, with a ${answers[1]} type of bean. ${answers[2]}, sent to me ${answers[4]}"`}</p>
-      ) : (
-        <p
-          className={orderSummaryStyles.content}
-        >{`"I drink my coffee as ${answers[0]}, with a ${answers[1]} type of bean. ${answers[2]} ground ala ${answers[3]}, sent to me ${answers[4]}"`}</p>
-      )}
+    // TODO: Use bg-order-summary.png as the background of the summary class
+    <div id="summary" className={orderSummaryStyles.summary}>
+      <div className={orderSummaryStyles.title}>ORDER SUMMARY</div>
+      <Content answers={answers} />
     </div>
   )
 }
