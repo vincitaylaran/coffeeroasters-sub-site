6d893c086c723f32c1c8acc30c60103ae49cd418
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-04 13:20:51 -0700

    Update styles for <Hero />

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,6 @@
 import { ReactChild, ReactNode } from "react"
 import heroStyles from "../styles/Hero.module.scss"
 
-import { useState, useEffect } from "react"
-
 type Background = "coffeepress" | "blackcup" | "whitecup"
 
 interface Props {
@@ -16,8 +14,8 @@ const Hero = ({ background, title, content, children }: Props) => {
   return (
     <div id={heroStyles[background]} className={heroStyles.container}>
       <div className={heroStyles.content}>
-        <h2>{title}</h2>
-        <p>{content}</p>
+        <h2 className={heroStyles.title}>{title}</h2>
+        <p className={heroStyles.description}>{content}</p>
         {children}
       </div>
     </div>
